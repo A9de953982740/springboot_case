007dcad961574c41e23bdb86e4cc55bc137dff91
A9de953982740 <953982740@qq.com>
2021-05-08 19:41:51 +0800

    增加新的文件

---
+++ src/main/java/com/atguigu/springboot/love.java
@@ -0,0 +1,11 @@
+package com.atguigu.springboot;
+
+/**
+ * @author cjhirom
+ * @create 2021-05-08 19:36
+ */
+public class love {
+    public static void main(String[] args) {
+        System.out.println("11111=" + 11111);
+    }
+}
